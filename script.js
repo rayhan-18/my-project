a3c73e0518c2f6da70af1caea313b2992936fe90
rayhan-18 <mr0031689@gmail.com>
2025-11-27 20:31:26 +0700

    tambah toggle dark

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-// script.js - demo interaksi form
+// Toggle Dark Mode
+const toggle = document.getElementById("toggleDark");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+});
+
+// Demo Form
 document.getElementById('contactForm').addEventListener('submit', function(e){
   e.preventDefault();
   const result = document.getElementById('formResult');
